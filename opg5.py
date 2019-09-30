5b8815e0986ad975442274c8d9b3e26b673bab21
Terhaug <terjehau95@gmail.com>
2019-09-30 12:50:24 +0100

    Added textfield to the task

--- opg5.py
+++ opg5.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
+import opg4
 
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # Trenger kode for orbit, runga-kutta og saturn V (Oppgave 2,3,4)
 
+#def acceleration(t, h, v, r = 6.378* (10 **6)):
+    # Må lage en funksjon som finner arealet av tverrsnittet basert på trinn:
+
+  #  area = Areal av tverrsnittfunk
+  #   m = opg4.estimate_mass(t)
+  #  acceleration = (opg4.get_thrust(t) / m) - (ForceOfGravity / m) - (air_res(h,area, v)/m)
